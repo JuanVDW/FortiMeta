7f062629a937a92afe61def40d09021667c4509b
Juan <43863325+JuanVDW@users.noreply.github.com>
2025-10-07 22:56:18 +0200

    Update fortimeta.py

--- fortimeta.py
+++ fortimeta.py
@@ -11,11 +11,9 @@ import streamlit as st
 
 col1, col2 = st.columns([1, 5])
 with col1:
-    st.image("logo.png", width=60)
+    st.image("logo.png", width=120)
 with col2:
-    st.markdown("<h1 style='padding-top: 10px;'>FortiMeta</h1>", unsafe_allow_html=True)
-
-st.title("❇️ Generate metadata variables")
+    st.markdown("<h1 style='padding-top: 10px;'>Generate metadata variables</h1>", unsafe_allow_html=True)
 
 # --- Files upload ---
 uploaded_excel = st.file_uploader("🗃️ Import the Excel file", type=["xlsx", "xls"])

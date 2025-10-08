0bcf843d6f00935f41c0eb47e75f921002bf1542
Juan <43863325+JuanVDW@users.noreply.github.com>
2025-10-08 22:19:31 +0200

    Update fortimeta.py

--- fortimeta.py
+++ fortimeta.py
@@ -14,7 +14,7 @@ with col2:
     st.markdown("<h1 style='margin-top: 7px;margin-left:-20px;'>Generate metadata variables</h1>", unsafe_allow_html=True)
 
 # --- File uploads ---
-st.markdown("### 📄 Upload Template file")  # plus grand texte
+st.markdown("<h2>📄 Upload Template file</h2>", unsafe_allow_html=True)
 uploaded_template = st.file_uploader("", type=["txt"])  # texte du label vide
 uploaded_excel = st.file_uploader("🗃️ Upload Excel file", type=["xlsx", "xls"])
 
